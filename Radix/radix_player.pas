597b0792c9ec9ea8e7e8b020a07abe52e5c68097
jval1972 <jvalavanis@gmail.com>
2020-05-15 07:08:44 +0300

    Do not play SndScrape sound in Doom levels when hiting floor

--- Radix/radix_player.pas
+++ Radix/radix_player.pas
@@ -666,8 +666,8 @@ begin
   pmo := p.mo;
   if p.planehittics <= 0 then
   begin
-    if Psubsector_t(pmo.subsector).sector.radixflags and RSF_RADIXSECTOR <> 0 then
-      if pmo.flags3_ex and MF3_EX_NOSOUND = 0 then
+    if pmo.flags3_ex and MF3_EX_NOSOUND = 0 then
+      if Psubsector_t(pmo.subsector).sector.radixflags and RSF_RADIXSECTOR <> 0 then
         S_AmbientSound(pmo.x, pmo.y, 'radix/SndScrape');
     p.planehittics := S_RadixSoundDuration(Ord(sfx_SndScrape));
   end;
@@ -764,7 +764,8 @@ begin
         if abs(p.mo.velz) > 8 * FRACUNIT then
         begin
           if p.mo.flags3_ex and MF3_EX_NOSOUND = 0 then
-            S_AmbientSound(p.mo.x, p.mo.y, 'radix/SndScrape');
+            if Psubsector_t(p.mo.subsector).sector.radixflags and RSF_RADIXSECTOR <> 0 then
+              S_AmbientSound(p.mo.x, p.mo.y, 'radix/SndScrape');
           p.planehittics := S_RadixSoundDuration(Ord(sfx_SndScrape));
           inc(p.wallhits, 2 * TICRATE);  // JVAL: 20200506 - Big penalty for bad pilot
           _spawn_burner_smoke_floor(-1, 16);  // Spawn more smoke to floor
@@ -773,7 +774,8 @@ begin
         else if abs(p.mo.velz) > 4 * FRACUNIT then
         begin
           if p.mo.flags3_ex and MF3_EX_NOSOUND = 0 then
-            S_AmbientSound(p.mo.x, p.mo.y, 'radix/SndScrape');
+            if Psubsector_t(p.mo.subsector).sector.radixflags and RSF_RADIXSECTOR <> 0 then
+              S_AmbientSound(p.mo.x, p.mo.y, 'radix/SndScrape');
           p.planehittics := S_RadixSoundDuration(Ord(sfx_SndScrape));
           inc(p.wallhits, TICRATE);  // JVAL: 20200506 - Small penalty for bad pilot
           _spawn_burner_smoke_floor(-1, 8);  // Spawn less smoke to floor
