e9562683057a999ca6fef1188e0e154b485214c5
jval1972 <jvalavanis@gmail.com>
2021-01-09 19:12:21 +0200

    Corrected DEHACKED export to ACTORDEF (renderstyle field)

--- Base/sc_actordef.pas
+++ Base/sc_actordef.pas
@@ -4,7 +4,7 @@
 //
 //  Copyright (C) 1995 by Epic MegaGames, Inc.
 //  Copyright (C) 1993-1996 by id Software, Inc.
-//  Copyright (C) 2004-2020 by Jim Valavanis
+//  Copyright (C) 2004-2021 by Jim Valavanis
 //
 //  This program is free software; you can redistribute it and/or
 //  modify it under the terms of the GNU General Public License
@@ -3025,7 +3025,7 @@ begin
     AddLn('Missileheight ' + S_GetSoundNameForNum(m.missileheight));
   if m.renderstyle <> mrs_normal then
   begin
-    AddLn('Rendersyle ' + renderstyle_tokens[Ord(m.renderstyle)]);
+    AddLn('Renderstyle ' + renderstyle_tokens[Ord(m.renderstyle)]);
     if m.alpha > 0 then
       AddLn('Alpha ' + ftoafmt('2.4', m.alpha / FRACUNIT));
   end;
