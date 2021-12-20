bd9dd800c23f80d3ae700729cb84b5f7887e70d2
jval1972 <jvalavanis@gmail.com>
2021-12-20 07:31:18 +0200

    Fixed compilation of rad2wad tool

--- rad2wad.dpr
+++ rad2wad.dpr
@@ -373,7 +373,8 @@ uses
   i_mainwindow in 'Base\i_mainwindow.pas',
   i_itmusic in 'Base\i_itmusic.pas',
   i_xmmusic in 'Base\i_xmmusic.pas',
-  s_externalmusic in 'Base\s_externalmusic.pas';
+  s_externalmusic in 'Base\s_externalmusic.pas',
+  radix_briefing in 'Radix\radix_briefing.pas';
 
 {$R *.res}
 
